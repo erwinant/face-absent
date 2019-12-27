c0524048237e7a43cb025091833e43dc41ca8dea
erwin antonius <erwin.ant@gmail.com>
2019-12-27 08:58:33 +0700

    change
    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,28 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as $ from 'jquery';
-import * as canvas from 'canvas';
 import * as faceapi from 'face-api.js';
-import { FaceExpressions, FaceDetection } from 'face-api.js';
-
-// patch nodejs environment, we need to provide an implementation of
-// HTMLCanvasElement and HTMLImageElement, additionally an implementation
-// of ImageData is required, in case you want to use the MTCNN
-const { Canvas, Image, ImageData } = canvas;
-// faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
-
-const TINY_FACE_DETECTOR = 'tiny_face_detector';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, AfterViewInit {
+export class AppComponent implements OnInit {
   title = 'face-absent';
   assetUrl = "http://localhost:4200/assets/";
   // tiny_face_detector options
   videoEl;
   stream;
   forwardTimes = [];
-  persons = ["erwin","kelvin"];
+  persons = ["erwin", "kelvin", "dirga", "elvira", "gusmita", "adi","diana"];
   faceMatcher = null;
   constructor() {
 
   }
 
-  async ngAfterViewInit() {
-
-    await faceapi.nets.tinyFaceDetector.load('http://localhost:4200/assets/weights');
-    await faceapi.nets.ssdMobilenetv1.load('http://localhost:4200/assets/weights');
-    //await faceapi.nets.ageGenderNet.load('http://localhost:4200/assets/weights');
-    await faceapi.nets.faceLandmark68Net.load('http://localhost:4200/assets/weights');
-    //await faceapi.nets.faceExpressionNet.load('http://localhost:4200/assets/weights');
-    await faceapi.nets.faceRecognitionNet.load('http://localhost:4200/assets/weights');
-    this.faceMatcher = await this.loadMatcher(2);
-    this.loadModels();
-  }
-  getFaceImageUri(name,idx) {
-    console.log(this.assetUrl + name+"/"+name + idx + ".png");
-    return this.assetUrl + name+"/"+name + idx + ".png";
+  getFaceImageUri(name, idx) {
+    console.log(this.assetUrl + name + "/" + name + idx + ".png");
+    return this.assetUrl + name + "/" + name + idx + ".png";
   }
 
   async loadMatcher(numImagesForTraining = 1) {
@@ -55,7 +34,7 @@ export class AppComponent implements OnInit, AfterViewInit {
         const descriptors = [];
         console.log(className);
         for (let i = 1; i < (numImagesForTraining + 1); i++) {
-          const img = await faceapi.fetchImage(this.getFaceImageUri(className,i))
+          const img = await faceapi.fetchImage(this.getFaceImageUri(className, i))
           descriptors.push(await faceapi.computeFaceDescriptor(img))
         }
         return new faceapi.LabeledFaceDescriptors(
@@ -64,7 +43,6 @@ export class AppComponent implements OnInit, AfterViewInit {
         )
       }
     ))
-    console.log(labeledFaceDescriptors);
     return new faceapi.FaceMatcher(labeledFaceDescriptors)
   }
   async loadModels() {
@@ -72,92 +50,98 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
   async cameraConnect() {
     this.stream = await navigator.mediaDevices.getUserMedia({ video: {} });
+
     this.videoEl = $('#videoEl').get(0);
     this.videoEl.srcObject = this.stream;
     if (this.videoEl.srcObject.active === true) {
       setTimeout(() => {
         this.startTrackingFace();
-      }, 1000);
+      }, 500);
 
     } else {
       console.log('Camera not connected...');
     }
   }
-  async startTracking() {
-    const videoEl = $('#videoEl').get(0);
+  // async startTracking() {
+  //   const videoEl = $('#videoEl').get(0);
 
-    const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 128, scoreThreshold: 0.5 });
-    const ts = Date.now();
+  //   const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 128, scoreThreshold: 0.5 });
+  //   const ts = Date.now();
 
-    const detections = await faceapi.detectAllFaces(videoEl, options)
-      .withAgeAndGender()
-      .withFaceExpressions();
+  //   const detections = await faceapi.detectAllFaces(videoEl, options)
+  //     .withAgeAndGender()
+  //     .withFaceExpressions();
 
-    if (videoEl.paused || videoEl.ended)
-      return setTimeout(() => this.startTracking());
+  //   if (videoEl.paused || videoEl.ended)
+  //     return setTimeout(() => this.startTracking());
 
-    const canvas = $('#overlay').get(0);
-    const minConfidence = 0.05
-    if (detections) {
-      const dims = faceapi.matchDimensions(canvas, videoEl, true);
-      const resizedResults = faceapi.resizeResults(detections, dims);
-      faceapi.draw.drawDetections(canvas, resizedResults);
-      faceapi.draw.drawFaceExpressions(canvas, resizedResults, minConfidence);
-    } else {
-      let w = canvas.width;
-      canvas.width = 10;
-      canvas.width = w;
-    }
-    setTimeout(() => this.startTracking());
-  }
+  //   const canvas = $('#overlay').get(0);
+  //   const minConfidence = 0.05
+  //   if (detections) {
+  //     const dims = faceapi.matchDimensions(canvas, videoEl, true);
+  //     const resizedResults = faceapi.resizeResults(detections, dims);
+  //     faceapi.draw.drawDetections(canvas, resizedResults);
+  //     faceapi.draw.drawFaceExpressions(canvas, resizedResults, minConfidence);
+  //   } else {
+  //     let w = canvas.width;
+  //     canvas.width = 10;
+  //     canvas.width = w;
+  //   }
+  //   setTimeout(() => this.startTracking());
+  // }
 
   async startTrackingFace() {
     const videoEl = $('#videoEl').get(0);
-
-    const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 512, scoreThreshold: 0.8 });
-    //const options = new faceapi.SsdMobilenetv1Options({ minConfidence: 0.1 });
+    const minConfidence = 0.05
+    //const options = new faceapi.TinyFaceDetectorOptions({ inputSize: 512, scoreThreshold: 0.8 });
+    const options = new faceapi.SsdMobilenetv1Options({ minConfidence: 0.5 });
 
     const detections = await faceapi.detectAllFaces(videoEl, options)
       .withFaceLandmarks()
-      .withFaceDescriptors()
+      .withFaceDescriptors().withFaceExpressions().withAgeAndGender()
 
     if (videoEl.paused || videoEl.ended)
-      return setTimeout(() => this.startTracking());
+      return setTimeout(() => this.startTrackingFace());
 
     const canvas = $('#overlay').get(0);
     // if (detections) {
     const dims = faceapi.matchDimensions(canvas, videoEl, true);
     // resize detection and landmarks in case displayed image is smaller than
     // original size
-    const resizedResults = faceapi.resizeResults(detections, dims)
+    const resizedResults = faceapi.resizeResults(detections, dims);
 
-    resizedResults.forEach(({ detection, descriptor }) => {
-      const fetchBest = this.faceMatcher.findBestMatch(descriptor);
-      if (fetchBest.label !== 'unknown') {
-        //const label = fetchBest.toString();
-        const label = fetchBest.label;
-        const options = { label }
-        const drawBox = new faceapi.draw.DrawBox(detection.box, options)
-        drawBox.draw(canvas)
+    resizedResults.forEach(({ detection, descriptor, age, gender, genderProbability }) => {
+      //faceapi.draw.drawFaceLandmarks(canvas, resizedResults);
+      
+      const fetchBest = this.faceMatcher.matchDescriptor(descriptor);
+      if (fetchBest.distance < 0.49) {
+        const label = fetchBest.toString();
+        const options = { label };
+        const drawBox = new faceapi.draw.DrawBox(detection.box, options);
+        faceapi.draw.drawFaceExpressions(canvas, resizedResults, minConfidence);
+        new faceapi.draw.DrawTextField(
+          [
+            `${faceapi.round(age, 0)} years`,
+            `${gender} (${faceapi.round(genderProbability)})`,
+
+          ],
+          detection.box
+        ).draw(canvas);
+        drawBox.draw(canvas);
       }
-
     })
-    // } else {
-    //   let w = canvas.width;
-    //   canvas.width = 10;
-    //   canvas.width = w;
-    // }
     setTimeout(() => this.startTrackingFace());
   }
 
-  ngOnInit() {
+  async ngOnInit() {
 
-    //await faceapi.nets.ssdMobilenetv1.loadFromUri('https://github.com/justadudewhohacks/face-api.js/tree/master/weights')
-    //const input = document.getElementById("myVideo") as HTMLVideoElement;
-    //const options = getFaceDetectorOptions();
-    //const detections = await faceapi.detectAllFaces(input).withAgeAndGender();
-    //console.log(detections);
-
-    // const canvas2 = faceapi.createCanvasFromMedia(document.getElementsByClassName("webcam-wrapper")[0].childNodes);
+    await faceapi.nets.tinyFaceDetector.load('http://localhost:4200/assets/weights');
+    await faceapi.nets.ssdMobilenetv1.load('http://localhost:4200/assets/weights');
+    await faceapi.nets.ageGenderNet.load('http://localhost:4200/assets/weights');
+    await faceapi.nets.faceLandmark68Net.load('http://localhost:4200/assets/weights');
+    await faceapi.nets.faceExpressionNet.load('http://localhost:4200/assets/weights');
+    await faceapi.nets.faceRecognitionNet.load('http://localhost:4200/assets/weights');
+    this.faceMatcher = await this.loadMatcher(1);
+    await this.cameraConnect();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {WebcamModule} from 'ngx-webcam';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    WebcamModule
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
